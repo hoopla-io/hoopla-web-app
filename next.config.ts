bdd81b01738efaa2d94e41c52d784be00eb4b474
lastninja294 <lastninja294@gmail.com>
2025-02-18 13:59:39 +0500

    fix image

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.hoopla.uz",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ['api.hoopla.uz'],
   },
 };
 
